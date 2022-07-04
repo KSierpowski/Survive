9cd565739cef7f0da6cae34789c1b7ec1eb036ec
Kacper Sierpowski <sierpowskikacper@gmail.com>
2022-07-04 20:04:19 +0200

    Added enemy attack

---
+++ Assets/Scripts/EnemyAttack.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAttack : MonoBehaviour
+{
+
+    [SerializeField] Transform target;
+    [SerializeField] float damage = 40f;
+
+
+
+    void Start()
+    {
+        
+    }
+
+    public void AttackHitEvent()
+    {
+        if (target == null)  return; 
+        Debug.Log("Hit");
+    }
+}
