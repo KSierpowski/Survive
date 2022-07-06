0e5abc4e79f378d2832ca39b63d1b37bce81c6a8
Kacper Sierpowski <sierpowskikacper@gmail.com>
2022-07-06 17:21:22 +0200

    Added enemy's face rotation

--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -7,6 +7,7 @@ public class EnemyAI : MonoBehaviour
 {
     [SerializeField] Transform target;
     [SerializeField] float chaseRange = 5f;
+    [SerializeField] float turnSpeed = 5f;
 
 
 
@@ -38,6 +39,7 @@ public class EnemyAI : MonoBehaviour
 
     void EngageTarget()
     {
+        FaceTarget();
         if (distanceToTarget >= navMeshAgent.stoppingDistance)
         {
             ChaseTarget();
@@ -57,10 +59,15 @@ public class EnemyAI : MonoBehaviour
     }
     void AttackTarget()
     {
-        GetComponent<Animator>().SetBool("attack", true);
-    
+        GetComponent<Animator>().SetBool("attack", true);   
     }
 
+    private void FaceTarget()
+    {
+        Vector3 direction = (target.position - transform.position).normalized;
+        Quaternion lookRotation = Quaternion.LookRotation(new Vector3 (direction.x, 0, direction.z));
+        transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * turnSpeed);
+    }
 
     void OnDrawGizmosSelected()
     {
