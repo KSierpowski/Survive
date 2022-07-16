d7aaa6c7c837bee23deeb6fad0b99f14ea54be73
Kacper Sierpowski <sierpowskikacper@gmail.com>
2022-07-16 10:57:29 +0200

    Fixed bug

--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -48,7 +48,7 @@ public class EnemyAI : MonoBehaviour
         {
             ChaseTarget();
         }
-        if (distanceToTarget >= navMeshAgent.stoppingDistance)
+        if (distanceToTarget <= navMeshAgent.stoppingDistance)
         {
             AttackTarget();
         }
