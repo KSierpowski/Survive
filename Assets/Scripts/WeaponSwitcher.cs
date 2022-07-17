e4fef9f0a924952c583c067b1cfdfd1a957dab30
Kacper Sierpowski <sierpowskikacper@gmail.com>
2022-07-17 12:06:18 +0200

    Added weapon switcher

--- Assets/Scripts/WeaponSwitcher.cs
+++ Assets/Scripts/WeaponSwitcher.cs
@@ -11,6 +11,54 @@ public class WeaponSwitcher : MonoBehaviour
     {
         SetWeaponActive();
     }
+    void Update()
+    {
+        int previousWeapon = currentWeapon;
+
+        ProcessKeyInput();
+        ProcessScreollWheel();
+
+        if(previousWeapon != currentWeapon)
+        {
+            SetWeaponActive();
+        }
+    }
+
+    private void ProcessScreollWheel()
+    {
+        if (Input.GetAxis("Mouse ScrollWheel") > 0)
+        {
+            if (currentWeapon >= transform.childCount - 1)
+            {
+                currentWeapon = 0;
+            }
+            else { currentWeapon++; }
+        }
+        if (Input.GetAxis("Mouse ScrollWheel") < 0)
+        {
+            if (currentWeapon <= 0)
+            {
+                currentWeapon = transform.childCount - 1;
+            }
+            else { currentWeapon--; }
+        }
+    }
+
+    private void ProcessKeyInput()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            currentWeapon = 0;
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            currentWeapon = 1;
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            currentWeapon = 2;
+        }
+    }
 
     private void SetWeaponActive()
     {
@@ -30,9 +78,6 @@ public class WeaponSwitcher : MonoBehaviour
         }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
+
 }
