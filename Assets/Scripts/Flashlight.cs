2cd0701b1472e35d697964d4cff460a459496a4e
Kacper Sierpowski <sierpowskikacper@gmail.com>
2022-07-29 13:21:06 +0200

    Added flashlight

---
+++ Assets/Scripts/Flashlight.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flashlight : MonoBehaviour
+{
+    [SerializeField] float lightDecay = .1f;
+    [SerializeField] float angleDecay = 1f;
+    [SerializeField] float minAngle = 40f;
+
+    Light myLight;
+
+    private void Start()
+    {
+        myLight = GetComponent<Light>();
+    }
+
+    private void Update()
+    {
+        DecreaseLightAngle();
+        DecreaseLightIntensity();
+    }
+
+
+    void DecreaseLightAngle()
+    {
+        if(myLight.spotAngle > minAngle)
+        {
+            myLight.spotAngle -= angleDecay * Time.deltaTime;
+
+        }
+        
+    }
+
+    void DecreaseLightIntensity()
+    {
+        myLight.intensity -= lightDecay * Time.deltaTime;
+
+    }
+
+
+
+
+
+
+
+
+}
